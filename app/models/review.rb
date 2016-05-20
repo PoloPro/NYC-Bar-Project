0d8b57285ae14400745adef2db28b5e27c9428f6
alexseth1124 <alexseth1124@gmail.com>
2016-05-20 10:55:22 -0400

    Users can add reviews, still need to set up AJAX

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :bar
+  validates :user, presence: true, uniqueness: true
+  validates :content, presence: true
+  validates :rating, inclusion: {in: [1, 2, 3, 4, 5]}
 end
