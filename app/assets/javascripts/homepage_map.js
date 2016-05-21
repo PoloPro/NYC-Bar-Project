b0b65a25a5fe9839805e23422c1c7875dbd22d41
RHOPRHH <rhoprhh@gmail.com>
2016-05-21 17:12:14 -0400

    map now has subways, a flyTo function, and bars list what subway stops are closest when clicked

--- app/assets/javascripts/homepage_map.js
+++ app/assets/javascripts/homepage_map.js
@@ -1,15 +1,15 @@
-var map;
-var mapgl;
-var ajaxRequest;
-var plotlist;
-var plotlayers=[];
 
+var mapgl;
+
+//CREATE MAP
 var initMapgl = function() {
+//create map boundaries
   var southWest = new mapboxgl.LngLat(-74.549, 40.261)
   var northEast = new mapboxgl.LngLat(-73.331, 41.062)
   var bounds = new mapboxgl.LngLatBounds(southWest, northEast)
+//access token
   mapboxgl.accessToken = 'pk.eyJ1IjoicmhvcHJoaCIsImEiOiJjaW9mN3J3OGYwMHN5eThtMnJ3enF0aHU4In0.cNczHe5-6C2mHIR5ivaKOw'
-
+//instantiate the map
   var mapgl = new mapboxgl.Map({
       container: 'mapgl',
       minZoom: 9,
@@ -22,7 +22,8 @@ var initMapgl = function() {
   });
 
 
-  var createMarkers = function() {
+//CREATE AND ADD all Bars to map
+  var createBarMarkers = function() {
     $.ajax({
       dataType: "JSON",
       url: '/markers/json'
@@ -44,7 +45,7 @@ var initMapgl = function() {
       })
     })
   }
-
+//CREATE AND ADD flatiron school to map
   var addFlatironSchool = function() {
     mapgl.addSource("flatironschool",
       { "type": "geojson",
@@ -71,28 +72,95 @@ var initMapgl = function() {
         "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
         "text-offset": [0, 0.6],
         "text-anchor": "top"
+      }
+    })
+  }
+//create subway markers
+  var createSubwayMarkers = function() {
+      $.ajax({
+        dataType: "JSON",
+        url: '/subways/json'
+      }).done(function(response){
+        var geos = JSON.parse(response)
+        mapgl.addSource("subways", geos)
+      })
     }
-  })
-}
+//add subways markers, called via button click
+  var addSubwayMarkers = function() {
+    mapgl.addLayer({
+      "id": "subways",
+      "type": "symbol",
+      "source": "subways",
+      "layout": {
+        "icon-image": "new-york-subway",
+        "text-field": "{name}",
+        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+        "text-offset": [0, 0.6],
+        "text-anchor": "top"
+      }
+    })
+  }
+//remove subway markers, called via button click
+  var removeSubwayMarkers = function() {
+    mapgl.removeLayer("subways")
+  }
 
+//listen to add and remove button
+  var buttonListener = function(){
+    $('#addsubway').click(function(){
+      addSubwayMarkers();
+    })
+    $('#removesubway').click(function(){
+      removeSubwayMarkers();
+    })
+  }
+
+// on click function for map popups for bars
 mapgl.on('click', function(e) {
-    var features = mapgl.queryRenderedFeatures(e.point, { radius: 10, layers: ['markers'] });
-    console.log(features[0].properties.title);
+    var features = mapgl.queryRenderedFeatures(e.point, { radius: 25, layers: ['markers'] });
+    if (features.length) {
+      mapgl.flyTo({
+        center: [e.lngLat.lng, e.lngLat.lat],
+        zoom: 14
+      })
+    }
+    var n = 70
+    var box = [[e.point.x - n, e.point.y - n], [e.point.x + n, e.point.y + n]]
+    var subways = mapgl.queryRenderedFeatures(box, { layers: ['subways'] });
+    var subs = subways.splice(0,2)
+    var subwaylist = "<ul>"
+    subs.forEach(function(station){
+      subwaylist += '<li>' + station.properties.name + " Station | Line(s): " + station.properties.line + '</li>'
+    })
+    subwaylist += '</ul>'
+    // console.log(subs)
+    // console.log(features[0].properties.title);
+    // console.log(features[0].properties.yelpid)
+    $.ajax({
+      type: "GET",
+      url: '/bars/' + features[0].properties.yelpid + '/mapclick'
+    }).done(function(response){
+      var tooltip = new mapboxgl.Popup({closeOnClick: true})
+        .setLngLat([response.longitude, response.latitude])
+        .setHTML('<center><h5>' + response.name +'</h5><p>' + response.address + '</p>' + subwaylist + '</center>')
+        .addTo(mapgl);
+      // either create a popup on the map at the bar location
+      // or drop down a card with the bar included
+    })
+  })
 
-      // mapgl.featuresAt(e.point, {radius: 1000, layer: 'markers', includeGeometry: true}, function(err, features) {
-      //       console.log(features[0]);
-      //
-      // });
-
-    });
-
-  mapgl.on("load", function(){
-    createMarkers();
+// adds markers when map loads
+mapgl.on("load", function(){
+    createBarMarkers();
+    createSubwayMarkers();
     addFlatironSchool();
+    buttonListener();
   })
 };
 
 
+
+//create map when page loads
 $(document).ready(function(){
   initMapgl();
 });
