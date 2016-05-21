7a0680cc7aa922f24af332b0ef8bc127be2e4eeb
RHOPRHH <rhoprhh@gmail.com>
2016-05-21 19:58:43 -0400

    added toggle button on subways, fixed repeating stations on closest list, and a few other small map fixes

--- app/assets/javascripts/homepage_map.js
+++ app/assets/javascripts/homepage_map.js
@@ -3,6 +3,8 @@ var mapgl;
 
 //CREATE MAP
 var initMapgl = function() {
+//create toggle variable
+  var toggle = false
 //create map boundaries
   var southWest = new mapboxgl.LngLat(-74.549, 40.261)
   var northEast = new mapboxgl.LngLat(-73.331, 41.062)
@@ -21,7 +23,6 @@ var initMapgl = function() {
       style: 'mapbox://styles/rhoprhh/cioegtr6d0011ainlkhuy28e8'
   });
 
-
 //CREATE AND ADD all Bars to map
   var createBarMarkers = function() {
     $.ajax({
@@ -37,7 +38,7 @@ var initMapgl = function() {
         "layout": {
           "icon-image": "bar-15",
           "text-field": "{title}",
-          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          "text-font": ["Elementa Pro Bold"],
           "text-offset": [0, 0.6],
           "text-anchor": "top"
         }
@@ -69,13 +70,13 @@ var initMapgl = function() {
       "layout": {
         "icon-image": "school-15",
         "text-field": "{title}",
-        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+        "text-font": ["Elementa Pro Bold"],
         "text-offset": [0, 0.6],
         "text-anchor": "top"
       }
     })
   }
-//create subway markers
+//creates our subway markers and removes included subway markers
   var createSubwayMarkers = function() {
       $.ajax({
         dataType: "JSON",
@@ -83,6 +84,7 @@ var initMapgl = function() {
       }).done(function(response){
         var geos = JSON.parse(response)
         mapgl.addSource("subways", geos)
+        mapgl.removeLayer('rail-label')
       })
     }
 //add subways markers, called via button click
@@ -94,7 +96,7 @@ var initMapgl = function() {
       "layout": {
         "icon-image": "new-york-subway",
         "text-field": "{name}",
-        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+        "text-font": ["Elementa Pro Bold"],
         "text-offset": [0, 0.6],
         "text-anchor": "top"
       }
@@ -104,7 +106,6 @@ var initMapgl = function() {
   var removeSubwayMarkers = function() {
     mapgl.removeLayer("subways")
   }
-
 //listen to add and remove button
   var buttonListener = function(){
     $('#addsubway').click(function(){
@@ -113,26 +114,47 @@ var initMapgl = function() {
     $('#removesubway').click(function(){
       removeSubwayMarkers();
     })
-  }
-
-// on click function for map popups for bars
-mapgl.on('click', function(e) {
-    var features = mapgl.queryRenderedFeatures(e.point, { radius: 25, layers: ['markers'] });
-    if (features.length) {
-      mapgl.flyTo({
-        center: [e.lngLat.lng, e.lngLat.lat],
-        zoom: 14
-      })
-    }
-    var n = 70
-    var box = [[e.point.x - n, e.point.y - n], [e.point.x + n, e.point.y + n]]
-    var subways = mapgl.queryRenderedFeatures(box, { layers: ['subways'] });
-    var subs = subways.splice(0,2)
-    var subwaylist = "<ul>"
-    subs.forEach(function(station){
-      subwaylist += '<li>' + station.properties.name + " Station | Line(s): " + station.properties.line + '</li>'
+    $('#togglesubway').click(function(){
+      if (toggle){
+        removeSubwayMarkers();
+        toggle = false
+        $('#togglesubway').html("Toggle Subways ON")
+      } else {
+        addSubwayMarkers();
+        toggle = true
+        $('#togglesubway').html("Toggle Subways OFF")
+      }
     })
-    subwaylist += '</ul>'
+  }
+// after map fly-to, then grab subways and add popups
+  var createPopup = function(e, features){
+    var n = 150
+    var box = [[e.point.x - n, e.point.y - n], [e.point.x + n, e.point.y + n]]
+    var subs = mapgl.queryRenderedFeatures(box, { layers: ['subways'] }).splice(0,6);
+    var subwaylist = ""
+    if (subs.length) {
+      subwaylist += "<h5>Nearby Subways</h5>"
+
+      var arrayBeforeUnique = []
+
+      subs.forEach(function(station){
+        arrayBeforeUnique.push('<p><strong>Station </strong>' + station.properties.name + "<strong>  |  Line(s): </strong>" + station.properties.line + '</p>')
+      })
+
+      function onlyUnique(value, index, self) {
+          return self.indexOf(value) === index;
+      }
+
+      var uniqueArray = arrayBeforeUnique.filter(onlyUnique)
+
+      uniqueArray.forEach(function(station){
+        subwaylist += station
+      })
+
+    } else {
+      subwaylist += "<h5>There are no subway stations nearby.</h5><h5>Or you have not added Subway markers onto the map.</h5>"
+    }
+
     // console.log(subs)
     // console.log(features[0].properties.title);
     // console.log(features[0].properties.yelpid)
@@ -142,20 +164,30 @@ mapgl.on('click', function(e) {
     }).done(function(response){
       var tooltip = new mapboxgl.Popup({closeOnClick: true})
         .setLngLat([response.longitude, response.latitude])
-        .setHTML('<center><h5>' + response.name +'</h5><p>' + response.address + '</p>' + subwaylist + '</center>')
+        .setHTML('<center><h5>' + response.name +'</h5><p>' + response.address + '</p><hr>' + subwaylist + '</center>')
         .addTo(mapgl);
       // either create a popup on the map at the bar location
       // or drop down a card with the bar included
     })
-  })
-
+  }
+// on click function for map popups for bars
+  mapgl.on('click', function(e) {
+      var features = mapgl.queryRenderedFeatures(e.point, { radius: 25, layers: ['markers'] });
+      if (features.length) {
+        mapgl.flyTo({
+          center: [e.lngLat.lng, e.lngLat.lat],
+          zoom: 16
+        })
+        createPopup(e, features);
+      }
+    })
 // adds markers when map loads
-mapgl.on("load", function(){
-    createBarMarkers();
-    createSubwayMarkers();
-    addFlatironSchool();
-    buttonListener();
-  })
+  mapgl.on("load", function(){
+      createBarMarkers();
+      createSubwayMarkers();
+      addFlatironSchool();
+      buttonListener();
+    })
 };
 
 
