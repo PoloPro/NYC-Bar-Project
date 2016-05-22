c10a87462c41348ba6610420f502e9bde39efef0
PoloPro <wesleybdavis@gmail.com>
2016-05-22 16:23:13 -0400

    pagination removed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree . 
-//
-// Bower packages
-//= require list.pagination.js/dist/list.pagination.js
--- app/assets/javascripts/bar-index-search.js
+++ app/assets/javascripts/bar-index-search.js
@@ -1,29 +1,10 @@
 $(document).ready(function() {
 
-  var paginationTopOptions = {
-    name: "paginationTop",
-    paginationClass: "paginationTop",
-    innerWindow: 1,
-    outerWindow: 1
-  }
-
-  var paginationBottomOptions = {
-    name: "paginationBottom",
-    paginationClass: "paginationBottom",
-    innerWindow: 1,
-    outerWindow: 1
-  }
-
   var listOptions = {
     valueNames: [ 'name', 'category' ],
-    page: 20,
-    plugins: [
-    ListPagination(paginationTopOptions),
-    ListPagination(paginationBottomOptions)
-    ]
   };
 
-  var wrapperElement = $('#bar-index')
+  var wrapperElement = $('#bar-list-searchable')
   var barIndexList = new List(wrapperElement[0], listOptions);
 
 })
