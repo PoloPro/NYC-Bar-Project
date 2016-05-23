20791c8dcbb10450e2985e504887ccfbf1c9d720
RHOPRHH <rhoprhh@gmail.com>
2016-05-23 18:54:56 -0400

    damn follow button

--- app/assets/javascripts/follow-button.js
+++ app/assets/javascripts/follow-button.js
@@ -1,18 +1,7 @@
-var unfollowHover = function(){
-  $(".btn-unfollow").hover(function(){
-    $(this).addClass('btn-danger').removeClass('btn-secondary')
-    $(this).val('Unfollow')
-  }, function(){
-    $(this).addClass('btn-secondary').removeClass('btn-danger')
-    $(this).val('Following')
-  })
-}
-
-  
-$(document).ready(function(){
-  unfollowHover()
-})
-
-$('.follow-button-area').on('click', '.btn', function(){
-  debugger
+$('.follow-button-area').hover(function(){
+  $('.btn-unfollow').addClass('btn-danger').removeClass('btn-secondary')
+  $('.btn-unfollow').val('Unfollow')
+}, function(){
+  $('.btn-unfollow').addClass('btn-secondary').removeClass('btn-danger')
+  $('.btn-unfollow').val('Following')
 })
