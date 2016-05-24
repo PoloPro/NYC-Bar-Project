edd4d1bdf514c97ff325c4a1d0d69d6047102f2b
PoloPro <wesleybdavis@gmail.com>
2016-05-24 15:58:11 -0400

    show images fixed

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -20,4 +20,23 @@ class Bar < ActiveRecord::Base
     reviews.count > 0 ? (average_rating * 2).round / 2.0 : "No ratings yet!"
   end
 
+  def self.random_interior_image
+    images = [
+      "https://media.timeout.com/images/100519023/image.jpg",
+      "https://greenvalleyranch.sclv.com/~/media/Images/Page-Background-Images/GVR/Entertainment/GV_Entertainment_LobbyBar_01new.jpg?h=630&la=en&w=1080",
+      "http://www.delkwoodgrill.net/wp-content/uploads/2016/01/SetWidth1700-Kempinski-Hotel-Bristol-Berlin-Gastronomie-Bristol-Bar.jpg",
+      "http://www.delkwoodgrill.net/wp-content/uploads/2016/01/bar-02.jpg",
+      "http://www.brusselspictures.com/wp-content/photos/DrugOpera1/drug.opera.1st.floor.bar.JPG",
+      "http://philly.thedrinknation.com/images/bars/ristorantepanoramaphilly.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/4/43/Bar-P1030319.jpg",
+      "http://esq.h-cdn.co/assets/cm/15/06/640x320/54d3cdbba4f40_-_esq-01-bar-lgn.jpg",
+      "http://i.huffpost.com/gen/1483434/images/o-BAR-facebook.jpg",
+      "http://images.teamsugar.com/files/upl1/1/17470/34_2008/art_divebar_01.preview.jpg",
+      "http://jackmovemag.com/wordpress/wordpress/wp-content/uploads/2011/09/Dive-Bar.jpg",
+      "http://images.huffingtonpost.com/2014-01-22-tiki3.jpg" 
+    ]
+
+    images.sample
+  end
+
 end
